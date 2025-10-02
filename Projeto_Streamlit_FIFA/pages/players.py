3588b89c09e41bd338ff98c49a7bda808fe715c2
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-02 13:41:49 -0300

    Update players.py

--- Projeto_Streamlit_FIFA/pages/players.py
+++ Projeto_Streamlit_FIFA/pages/players.py
@@ -1,43 +1,35 @@
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(
     page_title="Players",
     page_icon="🏃🏼",
     layout="wide"
 )
-
 df_data = st.session_state["data"]
 
-# Sidebar - seleção de clube
+
 clubes = df_data["Club"].value_counts().index
 club = st.sidebar.selectbox("Clube", clubes)
 
-# Filtra jogadores do clube
-df_players = df_data[df_data["Club"] == club]
+df_players = df_data[(df_data["Club"] == club)]
 players = df_players["Name"].value_counts().index
 player = st.sidebar.selectbox("Jogador", players)
 
-# Dados do jogador selecionado
 player_stats = df_data[df_data["Name"] == player].iloc[0]
 
-# --- FOTO DO JOGADOR ---
-if pd.notna(player_stats["Photo"]):
-    photo_url = str(player_stats["Photo"]).strip()
-    st.image(photo_url, width=150)  # garante exibição
-else:
-    st.write("📷 Foto não disponível")
-
-# --- INFORMAÇÕES BÁSICAS ---
+st.image(player_stats["Photo"])
 st.title(player_stats["Name"])
+
 st.markdown(f"**Clube:** {player_stats['Club']}")
 st.markdown(f"**Posição:** {player_stats['Position']}")
+st.write(repr(player_stats["Photo"]))
+st.write(type(player_stats["Photo"]))
+
 
 col1, col2, col3, col4 = st.columns(4)
 col1.markdown(f"**Idade:** {player_stats['Age']}")
-col2.markdown(f"**Altura:** {player_stats['Height(cm.)'] / 100:.2f} m")
-col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f} kg")
-
+col2.markdown(f"**Altura:** {player_stats['Height(cm.)'] / 100}")
+col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
 st.divider()
 
 st.subheader(f"Overall {player_stats['Overall']}")
